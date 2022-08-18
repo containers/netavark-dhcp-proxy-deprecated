fc3adb33aca0cc35d608502a66188261398b23cd
Jack <jackbaude@gmail.com>
2022-08-18 18:04:48 -0500

    Change build configuration
    Tonic build configuration makes it possible so that generated types from tonic can use attributes such as serialize and deserialize. This was necessary in order to marshal the lease cache information so that it can be saved on the fs.

--- build.rs
+++ build.rs
@@ -1,4 +1,27 @@
+extern crate core;
+
+use std::path::{PathBuf};
+
 fn main() {
-    tonic_build::compile_protos("proto/proxy.proto")
-        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+    let builder = tonic_build::configure()
+        .type_attribute("netavark_proxy.Lease", "#[derive(serde::Serialize)]")
+        .type_attribute("netavark_proxy.DhcpV4Lease", "#[derive(serde::Serialize)]")
+        .type_attribute("netavark_proxy.DhcpV6Lease", "#[derive(serde::Serialize)]")
+        .type_attribute("netavark_proxy.IPResponse", "#[derive(serde::Serialize)]")
+        .type_attribute("netavark_proxy.MacAddress", "#[derive(serde::Serialize)]")
+        .type_attribute("netavark_proxy.Ipv4Addr", "#[derive(serde::Serialize)]")
+        .type_attribute("netavark_proxy.Lease", "#[derive(serde::Deserialize)]")
+        .type_attribute("netavark_proxy.DhcpV4Lease", "#[derive(serde::Deserialize)]")
+        .type_attribute("netavark_proxy.DhcpV6Lease", "#[derive(serde::Deserialize)]")
+        .type_attribute("netavark_proxy.IPResponse", "#[derive(serde::Deserialize)]")
+        .type_attribute("netavark_proxy.MacAddress", "#[derive(serde::Deserialize)]")
+        .type_attribute("netavark_proxy.Ipv4Addr", "#[derive(serde::Deserialize)]")
+        .type_attribute("netavark_proxy.MacAddress", "#[derive(Eq)]")
+        .type_attribute("netavark_proxy.MacAddress", "#[derive(Hash)]")
+        .out_dir(PathBuf::from("src/grpc/"));
+
+    builder
+        .compile(&["proto/proxy.proto"], &["proto"])
+        .unwrap_or_else(|e| panic!("Failed to complie proto {:?}", e));
+
 }
---
+++ src/grpc/mod.rs
@@ -0,0 +1,1 @@
+pub mod netavark_proxy;
