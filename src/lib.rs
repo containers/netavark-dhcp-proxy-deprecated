d16dafe8c232529e3c5ed19484aae9f09a6fc7ba
Jack <jackbaude@gmail.com>
2022-08-19 00:36:12 -0500

    Move gRPC includes to netavark_proxy crate
    This change made it possible for every module in the crate to use the same instance of the generated gRPC types. Additionally the implementations for some of these types were added here to resolve E0117

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,62 @@
 pub mod cache;
-pub mod grpc;
+pub mod g_rpc {
+    include!("grpc/netavark_proxy.rs");
+    use mozim::DhcpV4Lease as MozimV4Lease;
+    impl From<MozimV4Lease> for Lease {
+        fn from(l: MozimV4Lease) -> Lease {
+            Lease {
+                ip_response: Some(IpResponse {
+                    t1: l.t1,
+                    t2: l.t2,
+                    lease_time: l.lease_time,
+                    mtu: 0,
+                    domain_name: l.domain_name,
+                    mac_addr: None,
+                }),
+                v4: Some(DhcpV4Lease {
+                    siaddr: Some(Ipv4Addr::from(l.siaddr)),
+                    yiaddr: Some(Ipv4Addr::from(l.yiaddr)),
+                    srv_id: Some(Ipv4Addr::from(l.srv_id)),
+                    subnet_mask: Some(Ipv4Addr::from(l.subnet_mask)),
+                    broadcast_addr: Some(Ipv4Addr::from(l.broadcast_addr)),
+                    dns_servers: handle_ip_vectors(l.dns_srvs),
+                    gateways: handle_ip_vectors(l.gateways),
+                    ntp_servers: handle_ip_vectors(l.ntp_srvs),
+                    host_name: l.host_name.unwrap_or(String::from("")),
+                }),
+                v6: None,
+            }
+        }
+    }
+
+    fn handle_ip_vectors(ip: Option<Vec<std::net::Ipv4Addr>>) -> Vec<Ipv4Addr> {
+        let mut ips: Vec<Ipv4Addr> = Vec::new();
+        if let Some(j) = ip {
+            for ip in j {
+                ips.push(Ipv4Addr::from(ip));
+            }
+        }
+        ips
+    }
+
+    impl From<std::net::Ipv4Addr> for Ipv4Addr {
+        fn from(ip: std::net::Ipv4Addr) -> Ipv4Addr {
+            return Ipv4Addr {
+                octets: Vec::from(ip.octets())
+            };
+        }
+    }
+
+    impl From<Option<std::net::Ipv4Addr>> for Ipv4Addr {
+        fn from(ip: Option<std::net::Ipv4Addr>) -> Self {
+            if let Some(addr) = ip {
+                return Ipv4Addr {
+                    octets: Vec::from(addr.octets())
+                };
+            }
+            return Ipv4Addr {
+                octets: Vec::from([0, 0, 0, 0])
+            };
+        }
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -88,90 +88,32 @@ fn get_client(network_config: NetworkConfig) -> Result<DhcpV4Client, DhcpError> 
             let client = DhcpV4Client::init(config, None)?;
             return Ok(client);
         }
-        //V6
+        //V6 TODO implement DHCPv6
         1 => {
             unimplemented!();
         }
         _ => {
-            return Err(DhcpError::new(mozim::ErrorKind::InvalidArgument, String::from("Must select a valid IP protocol 0=v4, 1=v6")))
+            return Err(DhcpError::new(ErrorKind::InvalidArgument, String::from("Must select a valid IP protocol 0=v4, 1=v6")));
         }
     }
 }
-impl NetavarkLease {
-    pub fn add_mac_addr(self, mac_addr: MacAddress) {
-        if let Some(mut ip_response) = self.ip_response {
-            ip_response.mac_addr = Some(mac_addr);
-        }
-    }
-}
+
 
 #[tokio::main]
 #[allow(unused)]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:10000".parse().unwrap();
+    let cache = match LeaseCache::new() {
+        Ok(c) => c,
+        Err(e) => {
+            log::warn!("IO error with the cache fs");
+            return Ok(());
+        }
+    };
     let netavark_proxy_service = NetavarkProxyService(LeaseCache::new().unwrap());
     Server::builder()
         .add_service(NetavarkProxyServer::new(netavark_proxy_service))
         .serve(addr)
         .await?;
     Ok(())
-}
-
-impl From<MozimV4Lease> for NetavarkLease {
-    fn from(l: MozimV4Lease) -> NetavarkLease {
-        NetavarkLease {
-            ip_response: Some(IpResponse {
-                t1: l.t1,
-                t2: l.t2,
-                lease_time: l.lease_time,
-                mtu: 0,
-                domain_name: l.domain_name,
-                mac_addr: None
-            }),
-            v4: Some(NetavarkV4Lease {
-                siaddr: Some(NetavarkIpv4Addr::from(l.siaddr)),
-                yiaddr: Some(NetavarkIpv4Addr::from(l.yiaddr)),
-                srv_id: Some(NetavarkIpv4Addr::from(l.srv_id)),
-                subnet_mask: Some(NetavarkIpv4Addr::from(l.subnet_mask)),
-                broadcast_addr: Some(NetavarkIpv4Addr::from(l.broadcast_addr)),
-                dns_servers: handle_ip_vectors(l.dns_srvs),
-                gateways: handle_ip_vectors(l.gateways),
-                ntp_servers: handle_ip_vectors(l.ntp_srvs),
-                host_name: l.host_name.unwrap_or(String::from("")),
-            }),
-            v6: None,
-
-        }
-    }
-}
-
-fn handle_ip_vectors(ip: Option<Vec<std::net::Ipv4Addr>>) -> Vec<NetavarkIpv4Addr> {
-    let mut ips: Vec<g_rpc::Ipv4Addr> = Vec::new();
-    if let Some(j) = ip {
-        for ip in j {
-            ips.push(g_rpc::Ipv4Addr::from(ip));
-        }
-    }
-    ips
-}
-
-impl From<std::net::Ipv4Addr> for NetavarkIpv4Addr {
-    fn from(ip: std::net::Ipv4Addr) -> NetavarkIpv4Addr {
-        return NetavarkIpv4Addr {
-            octets: Vec::from(ip.octets())
-        };
-    }
-}
-
-impl From<Option<std::net::Ipv4Addr>> for NetavarkIpv4Addr {
-    fn from(ip: Option<std::net::Ipv4Addr>) -> Self {
-        if let Some(addr) = ip {
-            return NetavarkIpv4Addr {
-                octets: Vec::from(addr.octets())
-            };
-        }
-        return NetavarkIpv4Addr {
-            octets: Vec::from([0, 0, 0, 0])
-        };
-    }
 }
